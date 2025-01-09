446b302a10953ff91aa1e498d82de72b1d066577
Tricogimmick <nomura@tricogimmick.com>
2025-01-09 10:47:32 +0900

    quotes周りの修正

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,18 @@ const articlesCollection = defineCollection({
       tags: z.array(z.string()),
     }),
 });
+
+const quotesCollection = defineCollection({ 
+  type: 'content', // v2.5.0以降
+  schema: z.object({
+    title: z.string(),
+    createdAt: z.date(),
+    source: z.string(),
+    description: z.string(),
+  }),
+});
+
 export const collections = {
   'articles': articlesCollection,
+  'quotes': quotesCollection,
 };

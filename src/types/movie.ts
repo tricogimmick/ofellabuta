3b7c5599923e1980c0e417c8946f3b3639f8089c
Tricogimmick <nomura@tricogimmick.com>
2025-01-10 16:44:49 +0900

    映画評を分離

---
+++ src/types/movie.ts
@@ -0,0 +1,12 @@
+export type MovieType = {
+    id: number | null;
+    title: string;
+    originalTitle: string;
+    seriesId: number | null;
+    country: string;
+    releaseYear: string;
+    description: string;
+    note: string;
+    viewingDate: string;
+    viewingMethod: string;
+};
